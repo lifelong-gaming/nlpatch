33182ad48b9b3a29e0ecdd955cc5b9235fc97d75
osoken <osoken.devel@outlook.jp>
2023-03-29 23:03:10 +0900

    feat(backend): add settings and basic package file

---
+++ backend/nlpatch/settings.py
@@ -0,0 +1,37 @@
+from abc import ABCMeta, abstractmethod
+from typing import TypeVar, Any
+import io
+import csv
+
+from pydantic import BaseSettings as _BaseSettings
+
+S = TypeVar("S", bound="BaseSettings")
+
+
+class BaseSettings(_BaseSettings):
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+        env_nested_delimiter = "__"
+
+
+class GlobalSettings(BaseSettings):
+    firebase_credentials: str
+    origins: list[str] = []
+
+    class Config:
+        @classmethod
+        def parse_env_var(cls, field_name: str, raw_val: str) -> Any:
+            if field_name == 'origins':
+                if raw_val == '':
+                    return []
+                return list(filter(lambda x: len(x) > 1, next(csv.reader(io.StringIO(raw_val)))))
+            return cls.json_loads(raw_val)
+
+
+
+class BaseComponentSettings(BaseSettings, metaclass=ABCMeta):
+    @classmethod
+    @abstractmethod
+    def from_global_settings(cls: type[S], settings: GlobalSettings) -> S:
+        raise NotImplementedError
