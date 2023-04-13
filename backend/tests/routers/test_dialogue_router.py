8a70d963eb67590d5ca1ac9cb78dd033a4959360
osoken <osoken.devel@outlook.jp>
2023-04-14 00:12:07 +0900

    test(backend): add invalid auth cases

--- backend/tests/routers/test_dialogue_router.py
+++ backend/tests/routers/test_dialogue_router.py
@@ -57,3 +57,25 @@ def test_retrieve_dialogue(
     response = client.get(f"/api/v1/dialogues/{dialogue_list[0].id}", headers={"Authorization": "Bearer valid_token"})
     assert response.status_code == 200
     assert response.json() == expected.dict()
+
+
+def test_retrieve_dialogue_returns_401_when_invalid_auth(
+    storage: BaseStorage, invalid_auth_provider: BaseAuthProvider, dialogue_ids: Sequence[str]
+) -> None:
+    sut = generate_dialogue_router(auth_provider=invalid_auth_provider, storage=storage)
+    app = FastAPI()
+    app.include_router(sut, prefix="/api/v1/dialogues")
+    client = TestClient(app)
+    response = client.get(f"/api/v1/dialogues/{dialogue_ids[0]}", headers={"Authorization": "Bearer invalid_token"})
+    assert response.status_code == 401
+
+
+def test_retrieve_dialogue_returns_401_when_no_auth_header(
+    storage: BaseStorage, valid_auth_provider: BaseAuthProvider, dialogue_ids: Sequence[str]
+) -> None:
+    sut = generate_dialogue_router(auth_provider=valid_auth_provider, storage=storage)
+    app = FastAPI()
+    app.include_router(sut, prefix="/api/v1/dialogues")
+    client = TestClient(app)
+    response = client.get(f"/api/v1/dialogues/{dialogue_ids[0]}")
+    assert response.status_code == 401
